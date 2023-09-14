70ded21cac0ab714b61a98fda78f02977c8f89e7
kailyn0215 <brownkai24@7hills.org>
2023-09-14 12:42:31 -0400

    did it!!!

---
+++ oldmacdonald.java
@@ -0,0 +1,11 @@
+public class oldmacdonald
+{
+    public void sing(String animal, String sound)
+    {
+    System.out.println("Old MacDonald had a farm, E-I-E-I-O!");
+    System.out.println("And on his farm he had a " + animal + ", E-I-E-I-O!");
+    System.out.println("With a " + sound + "-" + sound + " here, and a " + sound + "-" + sound + " there,");
+    System.out.println("here a " + sound + ", there a " + sound + ", everywhere a " + sound + "-" + sound + "!");
+    System.out.println("Old MacDonald had a farm, E-I-E-I-O!");
+    }
+}
---
+++ tester.java
@@ -0,0 +1,11 @@
+public class tester
+{
+    public static void main(String[] args)
+    {
+    oldmacdonald farmer = new oldmacdonald();
+	farmer.sing("pig", "oink");
+	farmer.sing("cow", "moo");
+    farmer.sing("horse", "neigh");
+    //farmer.sing("Julia", "ur mom");
+    }
+}
